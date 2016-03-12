afef550d7cdc9a9b87ab9696adeb0c3111de7b08
Humberto Calero <altairn5@uga.edu>
2016-03-11 18:29:06 -0800

    early mobile stages

--- js/app.js
+++ js/app.js
@@ -1,27 +1,71 @@
 $(document).ready(function(){
 
 
+var $profileImage;
+var $overlay=$("<div id='profile'></div>");
+var $photo= $("<img>");
+var $caption = $("<p></p>");
+var $mobile = $( window ).width();
 
-// var $manualModal = $('<div id="overlay"></div>');
+var $navbar = $(" <button type = 'button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>"
+              + "   <span class='sr-only'>Toggle navigation'</span> " 
+              + "   <span class='icon-bar'></span> "
+              + "   <span class='icon-bar'></span> "
+              + "   <span class='icon-bar'></span> "
+              + " </button> ");
 
-// var $profilePhoto = $('<img>');
-
-// $manualModal.append($profilePhoto);
-
-// $('body').append($manualModal);
-
-// $('#profile').on('click', '.img-responsive', function(e){
+$overlay.hide();
+$('body').append($overlay);
+$overlay.append($caption);
+$overlay.append($photo);
 
 
-// var photoPath = $(this).attr('src');
+function clickOnimage(e){
 
-// console.log(photoPath);
+	e.preventDefault();
 
-// $profilePhoto.attr('src', photoPath);
+	console.log('gere');
 
-// $manualModal.show();
+	$profileImage = $(this);
 
-// })
+	var capText = $profileImage.children('img').attr('atl');
+
+	var picturePath = $profileImage.attr('href');
+
+	$caption.text(capText)
+	$caption.css('color','white');
+ 	
+ 	$photo.attr('src', picturePath);	
+
+ 	$overlay.show();
+
+ 	$profileImage.hide();
+
+
+};
+
+
+$('.js-aboutme').on('click',clickOnimage);
+
+
+
+
+$overlay.on('click', function(){
+
+	$(this).hide();
+	$profileImage.show();
+	// $overlay.hide();
+})
+
+
+if($mobile< 768){
+
+	// $('.js-navbar-ul').hide();
+
+	// $('.js-navbar').append($navbar);
+
+}
+
 
 	
 })
